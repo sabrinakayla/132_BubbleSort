1fcc8fd3ea246c96481843ffa8a56aa4893bd20b
sabrinakayla <sabrinakuliah10@gmail.com>
2026-03-05 11:37:01 +0700

    procedur display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -49,3 +49,23 @@ void bubbeSortArray() {
 
     } while (pass <= n - 1);
 }
+
+
+void display() {
+    cout << endl;
+    cout << "==================================" << endl;
+    cout << "Masukkan Array yang telah tersusun" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
+    for (int j = 0; j < 0; j++) {
+        cout << arr[j];
+        if (j < n  - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
