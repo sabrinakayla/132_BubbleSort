a4934c60cbc1ebe5acd6f018a7138b7feb661b52
sabrinakayla <sabrinakuliah10@gmail.com>
2026-03-05 11:26:57 +0700

    procedur mengurutkan array

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -25,3 +25,27 @@ void input() {
         cin >> arr[i];
     }
 }
+
+void bubbeSortArray() {
+    int pass = 1; 
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j +1]) {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j = 1] = temp;
+            }
+        }
+        pass = pass + 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+
+        }
+        cout << endl;
+
+    } while (pass <= n - 1);
+}
